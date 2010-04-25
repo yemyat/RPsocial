032b383a7975d00e1840051643d4f21dd7bb1b0d
Ye Myat Min <yemyat@gmail.com>
2010-04-25 23:24:49 +0800

    database fix

--- db/migrate/20100421161831_create_posts.rb
+++ db/migrate/20100421161831_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def self.up
     create_table :posts do |t|
       t.integer :user_id
-      t.string :status
+      t.string :status,:limit=>600
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(:version => 20100425134322) do
 
   create_table "posts", :force => true do |t|
     t.integer  "user_id"
-    t.string   "status"
+    t.string   "status",     :limit => 600
     t.datetime "created_at"
     t.datetime "updated_at"
   end
